124fae0de4df18a996deddcd7bccad3975d984ca
AxelBremer <axelbremer94@gmail.com>
2017-12-15 15:22:42 +0100

    deleted Globals.py

--- Globals.py
+++
@@ -1,9 +0,0 @@
-class Globals():
-    def get_money_score(round_state):
-        p1 = round_state['seats'][0]
-        p2 = round_state['seats'][1]
-
-        total = p1['stack'] + p2['stack']
-        p1score = (1/total)*p1['stack']
-        p2score = (1/total)*p2['stack']
-        return p1score, p2score
