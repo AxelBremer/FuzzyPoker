e339976710e2743fb974e6092313b2fcafc215b2
timdh <tim7304@gmail.com>
2017-12-18 11:22:11 +0100

    error fix where me and opponent values were switched

--- FuzzyPokerPlayer.py
+++ FuzzyPokerPlayer.py
@@ -9,14 +9,14 @@ class FuzzyPokerPlayer(BasePokerPlayer):  # Do not forget to make parent class a
     def declare_action(self, valid_actions, hole_card, round_state):
         # valid_actions format => [raise_action_info, call_action_info, fold_action_info]
         call_action_info = valid_actions[1]
-        opponent = round_state['seats'][round_state['next_player']]
-        me = round_state['seats'][1-round_state['next_player']]
+        me = round_state['seats'][round_state['next_player']]
+        opponent = round_state['seats'][1-round_state['next_player']]
         aggro = calc.player_aggressiveness(round_state)[opponent['uuid']]
         tight = calc.player_tightness(round_state)[opponent['uuid']]
         money_opponent = calc.get_money_score(round_state)[opponent['uuid']]
         money_me = calc.get_money_score(round_state)[me['uuid']]
         winprob = calc.get_winprob(round_state, hole_card)
-        print(run_fuzzy_system(tight, aggro, money_opponent, money_me, winprob))
+        print('sys out', run_fuzzy_system(tight, aggro, money_opponent, money_me, winprob))
         action, amount = call_action_info["action"], call_action_info["amount"]
         return action, amount   # action returned here is sent to the poker engine
 
